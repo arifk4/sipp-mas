ee2399c50308ed52d8606eaaec3bf5bf72a47248
arifk4 <romy.vergyano@gmail.com>
2016-04-11 21:56:19 +0700

    Added files via upload

---
+++ assets/js/global.js
@@ -0,0 +1,21 @@
+$('.table > tbody > tr').click(function() {
+    $('#foot').slideToggle("up");
+});
+// $(function() {
+//     $("#content-wrapper").load("view/welcomepage.html");
+// });
+
+// function menu(dt) {
+//     if (dt == "pdj") {
+//         $(".page-content").load("view/pdj.html");
+//     } else if (dt == "home") {
+//         $(".page-content").load("view/main-content.html");
+//     } else if (dt == "asset"){
+//     	$(".page-content").load("view/asset.html");
+//     } else if (dt == "drawing"){
+//     	$(".page-content").load("view/drawing-mode.html");
+//     	$('.page-sidebar-menu.page-header-fixed').addClass('page-sidebar-menu-closed');
+//     	$('body').addClass('page-sidebar-closed');
+//     	$('.nav-link').addClass('disabled-link');
+//     }
+// }
